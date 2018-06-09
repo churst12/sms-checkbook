9d3fd6d4d5cbbbddaffa9aca9d6ee74101cdcbff
churst12 <churst@contextsmith.com>
2018-06-09 01:35:02 -0700

    plz work34

--- app.py
+++ app.py
@@ -29,10 +29,10 @@ def postDigital():
 	print "BEFORE BEFORE"
 	r = requests.post(url, data=jsonDict, headers={'Authorization':'6467326cd8274bb29b72d307cf39a9e9:bLCfg6rdOwMpCURUo77S52EzF78TMo'})
 	print "AFTER AFTER"
-	message = client.messages.create(
-    	to="+14087310723", 
-    	from_="+18317048704",
-    	body=r.text)
+	#message = client.messages.create(
+    #	to="+14087310723", 
+    #	from_="+18317048704",
+    #	body=r.text)
 	return r.txt
 	
 
