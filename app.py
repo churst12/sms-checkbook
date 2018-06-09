858460a9c326abae3bd3fe586307fdc528aa5b4b
churst12 <churst@contextsmith.com>
2018-06-08 23:46:20 -0700

    plz work21

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ def postDigital():
 			"amount": 10.42
 			}
 	#r = request.post( url, headers=headers, data=data)
+	print "BEFORE BEFORE"
 	r = requests.post(url, data=data, auth=('1e55402a63d64eeea4d633f0efcb432d:WnyNnTkSN6PY4Be1fk99KHGN5yVMEB'))
 	print "got past r="
 	return r.text
