ae1a48d0449d9cf41c4b7f4eb949b66d511eec2c
churst12 <churst@contextsmith.com>
2018-06-08 23:45:16 -0700

    plz work20

--- app.py
+++ app.py
@@ -23,6 +23,7 @@ def postDigital():
 			}
 	#r = request.post( url, headers=headers, data=data)
 	r = requests.post(url, data=data, auth=('1e55402a63d64eeea4d633f0efcb432d:WnyNnTkSN6PY4Be1fk99KHGN5yVMEB'))
+	print "got past r="
 	return r.text
 	
 
