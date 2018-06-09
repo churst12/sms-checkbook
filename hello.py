b9461fed098b33c19954057d5cbb00edf0b31d66
Collin Hurst <churst@Checkbooks-MacBook-Pro.local>
2018-06-08 19:51:52 -0700

    close

--- hello.py
+++ hello.py
@@ -1,11 +1,27 @@
 import os
 from flask import Flask
+import json
+import requests
+from flask import render_template
+
+Authorization= '23566e2b3f2d26921e2355bfd03497ee','16640e2b05a59751b7bf923737cb9942'
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello world!"
+    return render_template('index.html', title='Home')
+
+@app.route("/send")
+def postDigital():
+	endpoint = "https://sandbox.checkbook.io/v3/check/digital"
+	info = {"name":"Test Inc.",
+			"recipient":"widgets@example.com", 
+			"amount": 10.42
+			}
+	r = request.post( endpoint, info)
+	#return r.text
+	return "HI"
 
 if __name__ == "__main__":
     #port = int(os.environ.get("PORT", 5000))
